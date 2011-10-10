70e0fa1befb8b6b5d4eb1185eca80db976800a3a
FrozenData <FrozenData@gmail.com>
2011-10-11 01:04:25 +0530

    Tweaked cfq scheduler - For better IO response

--- Initramfs/sbin/boot/tweaks.sh
+++ Initramfs/sbin/boot/tweaks.sh
@@ -15,4 +15,15 @@ if /sbin/busybox [ "`/sbin/busybox grep ANDROIDLOGGER /system/etc/tweaks.conf`" 
   insmod /lib/modules/logger.ko
 fi
 
+# Tweak cfq io scheduler
+  for i in $(/sbin/busybox ls -1 /sys/block/mmc*)
+  do echo "0" > $i/queue/rotational
+    echo "0" > $i/queue/iostats
+    echo "1" > $i/queue/iosched/group_isolation
+    echo "4" > $i/queue/iosched/quantum
+    echo "1" > $i/queue/iosched/low_latency
+    echo "5" > $i/queue/iosched/slice_idle
+    echo "2" > $i/queue/iosched/back_seek_penalty
+    echo "1000000000" > $i/queue/iosched/back_seek_max
+  done
 
